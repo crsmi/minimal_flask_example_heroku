4604fc17173fcaf29a947e6fde582d53e101e3a1
Collin Smith <crsmith2@uwalumni.com>
2019-04-18 13:12:49 -0500

    Updated to AUDL forecast app.

--- script.py
+++ script.py
@@ -1,9 +1,35 @@
-from flask import Flask
-from serve import useless_function  
+import json
+from flask import Flask, request
+from serve import get_forecast_api
 
-app=Flask(__name__)
+#from serve import get_keywords_api
+# I've commented out the last import because it won't work in kernels,
+# but you should uncomment it when we build our app tomorrow
 
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
+# create an instance of Flask
+app = Flask(__name__)
+
+# load our pre-trained model & function
+forecast_api = get_forecast_api()
+
+# Define a post method for our API.
+@app.route('/forecast', methods=['POST'])
+def forecast():
+    """
+    Takes in a json file, extracts the keywords &
+    their indices and then returns them as a json file.
+    """
+    # the data the user input, in json format
+    input_data = request.json
+
+    # use our API function to get the keywords
+    output_data = forecast_api(input_data)
+
+    # convert our dictionary into a .json file
+    # (returning a dictionary wouldn't be very
+    # helpful for someone querying our API from
+    # java; JSON is more flexible/portable)
+    response = json.dumps(output_data)
+
+    # return our json file
+    return response
--- serve.py
+++ serve.py
@@ -1,5 +1,19 @@
-def useless_function():
-    """ 
-    A function that returns some text
-    """
-    return("some text")
+import pickle
+
+# Function that takes loads in our pickled word processor
+# and defines a function for using it. This makes it easy
+# to do these steps together when serving our model.
+def get_forecast_api():
+
+    # read in pickled word processor. You could also load in
+    # other models as this step.
+    f = pickle.load(open("forecast.pkl", "rb"))
+
+    # Function to apply our model & extract keywords from a
+    # provided bit of text
+    def forecast_api(h,a):
+        forecast = f(h,a)
+        return forecast
+
+    # return the function we just defined
+    return forecast_api
